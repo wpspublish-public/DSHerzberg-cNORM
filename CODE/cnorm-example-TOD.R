23f79a146f0db08045eac574d949913fd0edb3f7
David Herzberg <dherzberg@wpspublish.com>
2021-10-31 20:01:13 -0700

    weighting example

---
+++ CODE/cnorm-example-TOD.R
@@ -0,0 +1,213 @@
+library(cNORM)
+suppressMessages(suppressWarnings(library(tidyverse)))
+suppressMessages(library(here))
+library(writexl)
+suppressMessages(library(lubridate))
+
+# Prep input data file. Parse to three cols: personID, group (explanatory var  -
+# age), raw score
+
+# General tokens
+combined_score_to_norm_file_name <- "TODE_8.27.21_fornorms-weighted-sum-scores.csv"
+input_file_path <- "INPUT-FILES/NORMS/TODE_8.27.21_fornorms/"
+output_file_path <- "OUTPUT-FILES/NORMS/TODE_8.27.21_fornorms/"
+
+# Tokens to toggle between using weighted vs. unweighted scores as the basis for
+# the norms.
+
+# scores <- c("sege_sum_w", "rlne_sum_w", "rhme_sum_w", "snwe_sum_w",
+# "lswe_sum_w", "lske_sum_w", "ORF_noNeg_w")
+scores <- c("sege_sum", "rlne_sum", "rhme_sum", "snwe_sum",
+            "lswe_sum", "lske_sum", "ORF_noNeg")
+
+# Tokens setting the specific score to be normed on this iteration of the
+# script.
+score_to_norm_stem <- "rhme_sum"
+score_to_norm_file_name <- str_c(score_to_norm_stem, "-norms-input.csv")
+score_to_norm_max_raw <- data.frame(test = score_to_norm_stem) %>%
+  mutate(
+    max_raw = case_when(
+      str_detect(test, "sege") ~ 25,
+      str_detect(test, "rlne") ~ 120,
+      str_detect(test, "rhme") ~ 30,
+      str_detect(test, "snwe") ~ 32,
+      str_detect(test, "lswe") ~ 38,
+      str_detect(test, "lske") ~ 33,
+      str_detect(test, "ORF_noNeg") ~ 263
+    )
+  ) %>%
+  pull(max_raw)
+
+
+# to use age as predictor in cNORM, read in DOB, date_admin, calculate
+# chronological age as decimal value.
+age_contin <- suppressMessages(read_csv(here(
+  str_c(input_file_path, "TODE_8.27.21_fornorms.csv")
+))) %>% 
+  mutate(
+    across(
+      c(DOB, admin_date),
+      ~
+        # lubridate::mdy() coerces string into date-time type
+        mdy(.x)
+    ),
+    # [start_date] %--% [end_date] uses lubridate operator %--% to create a time
+    # interval (duration). Dividing interval by a period (years(1) = 1 year)
+    # returns age in years-months-days as a decimal value (e.g., 5 years, 6
+    # months, 0 days = 5.5)
+    age = (DOB %--% admin_date) / years (1)
+  ) %>%
+  # here bind_cols joins a col created by cNORM::getGroups(), which returns vec
+  # with equal size age strata, where value of col is a label for the age stratum that
+  # each row belongs to, and that label is the arithmetic mean of chronological
+  # age within that group
+  bind_cols(getGroups(.$age)) %>% 
+  rename(group = ...51) %>% 
+  select(ID, age, group)
+
+# write versions of age, group vars for consultant Alex Lenhard.
+age_contin %>% 
+  select(ID, age) %>% 
+  write_csv(here(
+    str_c(input_file_path, "TOD-AL-age-contin.csv")
+  ))
+
+age_contin %>% 
+  rename(getGroup = group) %>% 
+  select(ID, age, getGroup) %>% 
+  write_csv(here(
+    str_c(input_file_path, "TOD-AL-age-contin-getGroup.csv")
+  ))
+
+
+# Next block reads an input containing multiple raw score columns per person,
+# processes into separate dfs that are input files into cNORM for norming one
+# raw score. Within the process, a list of dfs is created and those dfs are
+# eventually written out as .csvs, but the list itself is invisible, it's never
+# preserved in the global environment. These new input files for cnorm()
+# incorporate the continuous age variable and age grouping variable created in
+# the previous step.
+map(
+  scores,
+  ~
+    suppressMessages(read_csv(here(
+      str_c(input_file_path, combined_score_to_norm_file_name)
+    ))) %>%
+    select(ID, !!sym(.x)) %>%
+    drop_na(!!sym(.x)) %>% 
+    left_join(age_contin, by = "ID") %>% 
+    rename(raw = !!sym(.x)) %>% 
+    select(ID, age, group, raw)
+) %>%
+  set_names(scores) %>%
+  map2(scores,
+       ~
+         write_csv(.x,
+                   here(
+                     str_c(input_file_path, .y, "-norms-input.csv")
+                   ))) %>% 
+  invisible(.)
+
+# read single score input.
+
+input <- suppressMessages(read_csv(here(str_c(
+  input_file_path, score_to_norm_file_name
+))))
+
+# Alex Lenhard's recommended approach
+
+# Use the all-in-one cnorm() function to create the model.
+# Compare diagnostics from two approaches to defining age groups: -
+# 1. use getGroups() to create equal size groups out of the age distributions,
+# use "groups = " argument within cnorm() to refer to column holding this
+# grouping code.
+# 2. omit group argument: cnorm() defaults to rankBySlidingWindow, but this can
+# be problematic when there are few cases on the tails of the age distribution -
+
+# The two key diagnostics are plot(model, "series") and checkConsistency(). Both
+# target the same problem: violations of monotonicty, or intersecting percentile
+# curves. With plot(model, "series"), you can use "end" argument to set upper
+# limit of predictors.
+
+model <- cnorm(raw = input$raw, group = input$group, k = 4, terms = 5, scale = "IQ")
+# model <- cnorm(raw = input$raw, age = input$age, width = 1, k = 4, terms = 4, scale = "IQ")
+plot(model, "series", end = 10)
+checkConsistency(model)
+
+#Once you have the model, cNORM allows you generate post hoc age groups of any
+#width, centers on any age points you choose. The tab_names token is used for
+#writing out a multi-tabbed norms table, the labels in this token give the lower
+#bound of each age range.
+tab_names <- c("5.0", "5.6", "6.0", "6.6", "7.0", "7.6", "8.0")
+
+# Prepare a list of data frames, each df is raw-to-ss lookup table for an age group.
+norms_list <- rawTable(
+  c(5.25, 5.75, 6.25, 6.75, 7.25, 7.75, 8.25), 
+  model, 
+  step = 1, 
+  minNorm = 40, 
+  maxNorm = 130, 
+  minRaw = 1, 
+  maxRaw = score_to_norm_max_raw,
+  pretty = TRUE
+  ) %>% 
+  set_names(tab_names) %>% 
+  map( 
+    ~
+      select(.x, raw, norm) %>% 
+      summarize(raw = raw,
+                ss = round(norm, 0))
+)
+
+# prepare reversal report
+reversal_report <- norms_list %>%
+  reduce(left_join,
+         by = "raw") %>%
+  set_names("raw", tab_names) %>%
+  pivot_longer(-raw, names_to = "agestrat", values_to = "ss") %>%
+  group_by(raw) %>%
+  mutate(reversal = case_when(lag(ss) < ss ~ 1)) %>%
+  filter(reversal == 1) %>%
+  select(raw, agestrat) %>%
+  write_csv(here(
+    str_c(output_file_path, score_to_norm_stem, "-reversal-report.csv")
+  ))
+
+
+# Write raw-to-ss lookups by agestrat into tabbed, xlsx workbook. To create
+# named tabs, supply writexl::write_xlsx() with a named list of dfs for each
+# tab, tab names will be the names of the list elements
+write_xlsx(norms_list,
+           here(str_c(
+             output_file_path, score_to_norm_stem, "-raw-ss-lookup-tabbed.xlsx"
+           )))
+
+# write model summary to text file, so you can replicate model later.
+capture.output(
+  summary(model),
+  file = here(
+    str_c(output_file_path, score_to_norm_stem, "-model-summ.txt")  )
+)
+
+
+# write raw-to-ss-lookups to single-sheet table
+table <- norms_list %>%
+  reduce(left_join,
+         by = "raw") %>%
+  set_names("raw", tab_names)
+
+write_csv(table, 
+          here(
+  str_c(output_file_path, score_to_norm_stem, "-raw-ss-lookup-table.csv")
+))
+
+# write model summary to text file, so you can replicate model later.
+capture.output(
+  str_c(score_to_norm_stem, " model summary"), 
+  summary(model),
+  file = here(
+    str_c(output_file_path, score_to_norm_stem, "-model-summ.txt")  )
+)
+
+
+
---
+++ CODE/weighted-ranking-example-WL.R
@@ -0,0 +1,66 @@
+# Test for weighting function in cNORM
+# Procedure: Simulate population with two differing samples, e. g. 80% vs. 20% with
+# different mean and sd
+#
+# First, some population parameters
+# n.population specifies the size of the completely representative population
+# n.normsample is the smaller norming sample, drawn randomly
+# Later on, both groups will be drawn with a 50% share each and the percentiles
+# will be estimated without weighting (red) or with weighting (blue)
+# The other parameters can be set at will.
+library(cNORM)
+
+n.population <- 100000
+n.normsample <- 1000
+
+m1 <- 12
+sd1 <- 3
+percentage1 <- .8
+
+m2 <- 8
+sd2 <- 3
+percentage2 <- .2
+
+
+# now, let's role the dice
+# generate a large representative sample with 100 000 cases
+n1 <- n.population*percentage1
+sample1 <- data.frame(m=rep(m1, length.out=n1), percentile=seq(from=(.5/n1), to=(n1-.5) / n1, length.out=n1), weight=percentage1*100)
+sample1$z <- qnorm(sample1$percentile, m1, sd1)
+
+n2 <- n.population*percentage2
+sample2 <- data.frame(m=rep(m2, length.out=n2), percentile=seq(from=(.5/n2), to=(n2-.5) / n2, length.out=n2), weight=percentage2*100)
+sample2$z <- qnorm(sample2$percentile, m2, sd2)
+
+totalSample <- rbind(sample1, sample2)
+totalSample$percentileTotal <- rank(totalSample$z)/(n1+n2)
+totalSample$zTotal <- qnorm(totalSample$percentileTotal)
+totalSample <- totalSample[order(totalSample$zTotal),]
+totalSample$raw <- totalSample$z
+
+# draw unstratified sample, groups occur with equal share
+n.b <- n.normsample
+sample1.b <- data.frame(m=rep(m1, length.out=n.b/2), z=rnorm(n.b/2, m1, sd1), weight=n.b/2)
+sample1.b$percentile <- pnorm((sample1.b$z - m1)/sd1)
+sample2.b <- data.frame(m=rep(m2, length.out=n.b/2), z=rnorm(n.b/2, m2, sd2), weight=n.b/2)
+sample2.b$percentile <- pnorm((sample2.b$z - m2)/sd2)
+total2.b <- rbind(sample1.b, sample2.b)
+total2.b <- total2.b[order(total2.b$z),]
+totalSample2 <- rankByGroup(total2.b, raw="z", group=FALSE)
+
+# draw unstratified sample, but use weighting
+n.c <- n.normsample
+sample1.c <- data.frame(m=rep(m1, length.out=n.c/2), z=rnorm(n.c/2, m1, sd1), weight=100 * percentage1)
+sample1.c$percentile <- pnorm((sample1.c$z - m1)/sd1)
+sample2.c <- data.frame(m=rep(m2, length.out=n.c/2), z=rnorm(n.c/2, m2, sd2), weight=100 * percentage2)
+sample2.c$percentile <- pnorm((sample2.c$z - m2)/sd2)
+total2.c <- rbind(sample1.c, sample2.c)
+total2.c <- total2.c[order(total2.c$z),]
+totalSample3 <- rankByGroup(total2.c, raw="z", group=FALSE, weights = "weight")
+
+# plot
+plot(totalSample$raw, totalSample$percentileTotal, type="l")
+lines(totalSample2$z, totalSample2$percentile, col="red")
+lines(totalSample3$z, totalSample3$percentile, col="blue")
+legend(1, 1, legend=c("Population", "unweighted percentile", "weighting function"), col=c("black", "red", "blue"), lty=1:3, cex=0.8)
+
