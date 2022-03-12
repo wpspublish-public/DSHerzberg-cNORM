3e57912653c2df3a88d895d90da61f9db591a415
David Herzberg <dherzberg@wpspublish.com>
2022-03-11 17:03:59 -0800

    documentation through pivot_wider()

--- CODE/scratch.R
+++ CODE/scratch.R
@@ -4,7 +4,14 @@ temp1 <- input_files_ta[[6]] %>%
         group_by(age_strat) %>%
         complete(ss = 40:130) %>%
         group_by(age_strat, ss) %>% 
-        filter(n() == 1 | n() > 1 & row_number()  %in% c(1, n()))
+        filter(n() == 1 | n() > 1 & row_number()  %in% c(1, n())) %>% 
+        summarize(raw = str_c(raw, collapse = '--')) %>%
+        mutate(across(raw, ~ case_when(is.na(.x) ~ '-', TRUE ~ .x))) %>%
+        arrange(age_strat, desc(ss)) %>%
+        pivot_wider(names_from = age_strat,
+                    values_from = raw) %>%
+        filter(!is.na(ss))
+        
 
 temp2 <- temp1[80:95, ]
 
